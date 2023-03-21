71f313a073ef5d12895bc9f3d2aeae8609d00087
friol <dantonag@gmail.com>
2023-03-21 20:38:54 +0100

    Info hover

--- js/main.js
+++ js/main.js
@@ -34,41 +34,11 @@ var glbVdpMode=0; // 0 NTSC, 1 PAL
 
 //
 
-function getDataUrlFromArr(arr, w, h) 
-{
-    if(typeof w === 'undefined' || typeof h === 'undefined') {
-        w = h = Math.sqrt(arr.length / 4);
-    }
-
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
-
-    canvas.width = w;
-    canvas.height = h;
-
-    const imgData = ctx.createImageData(w, h);
-    imgData.data.set(arr);
-    ctx.putImageData(imgData, 0, 0);
-
-    return canvas.toDataURL();
-}
-  
-function getImgFromDataUrl(data) 
-{
-    const img = document.createElement('img');
-    img.src = data;
-    return img;
-}
-  
-function getImgFromArr(arr, w, h) 
-{
-    return getImgFromDataUrl(getDataUrlFromArr(arr, w, h));
-}
-
 function loadSavestateImage()
 {
     var uintarr=new Uint8ClampedArray(256*240*4);
     var imgAsArray=JSON.parse(localStorage.getItem('savestateScreenshot'));
+    if (imgAsArray==null) return;
 
     const canvas2 = document.createElement('canvas');
     canvas2.width=256;
